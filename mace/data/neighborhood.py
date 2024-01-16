849a906521d2ffb06f26436f5b0ef07f775e6f4c
felix musil <musil.felix@gmail.com>
2024-01-16 16:59:00 +0100

    simpler handdling of non periodic cells

--- mace/data/neighborhood.py
+++ mace/data/neighborhood.py
@@ -13,26 +13,32 @@ def get_neighborhood(
 ) -> Tuple[np.ndarray, np.ndarray]:
     if pbc is None:
         pbc = (False, False, False)
+    cell_origin = None
+    if cell is None:
+        rmin = np.min(positions, axis=0)
+        rmax = np.max(positions, axis=0)
+        cell_origin = rmin
+        cell = np.diag(rmax - rmin)
+    else:
+        if cell is None or cell.any() == np.zeros((3, 3)).any():
+            cell = np.identity(3, dtype=float)
 
-    if cell is None or cell.any() == np.zeros((3, 3)).any():
-        cell = np.identity(3, dtype=float)
+        assert len(pbc) == 3 and all(isinstance(i, (bool, np.bool_)) for i in pbc)
+        assert cell.shape == (3, 3)
 
-    assert len(pbc) == 3 and all(isinstance(i, (bool, np.bool_)) for i in pbc)
-    assert cell.shape == (3, 3)
-
-    pbc_x = pbc[0]
-    pbc_y = pbc[1]
-    pbc_z = pbc[2]
-    identity = np.identity(3, dtype=float)
-    max_positions = np.max(np.absolute(positions)) + 1
-    # Extend cell in non-periodic directions
-    # For models with more than 5 layers, the multiplicative constant needs to be increased.
-    if not pbc_x:
-        cell[:, 0] = max_positions * 5 * cutoff * identity[:, 0]
-    if not pbc_y:
-        cell[:, 1] = max_positions * 5 * cutoff * identity[:, 1]
-    if not pbc_z:
-        cell[:, 2] = max_positions * 5 * cutoff * identity[:, 2]
+        pbc_x = pbc[0]
+        pbc_y = pbc[1]
+        pbc_z = pbc[2]
+        identity = np.identity(3, dtype=float)
+        max_positions = np.max(np.absolute(positions)) + 1
+        # Extend cell in non-periodic directions
+        # For models with more than 5 layers, the multiplicative constant needs to be increased.
+        if not pbc_x:
+            cell[:, 0] = max_positions * 5 * cutoff * identity[:, 0]
+        if not pbc_y:
+            cell[:, 1] = max_positions * 5 * cutoff * identity[:, 1]
+        if not pbc_z:
+            cell[:, 2] = max_positions * 5 * cutoff * identity[:, 2]
 
     sender, receiver, unit_shifts = neighbour_list(
         quantities="ijS",
@@ -40,6 +46,7 @@ def get_neighborhood(
         cell=cell,
         positions=positions,
         cutoff=cutoff,
+        cell_origin=cell_origin,
         # self_interaction=True,  # we want edges from atom to itself in different periodic images
         # use_scaled_positions=False,  # positions are not scaled positions
     )
